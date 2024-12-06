a4d787ee3e80ebdd0a06318e02ab1730fd3431fd
Nicolò Penserini - nicolo.penserini@studio.unibo.it <nicolo.penserini@studio.unibo.it>
2024-12-06 10:29:19 +0100

    Solved ES 01

--- login.php
+++ login.php
@@ -1,8 +1,45 @@
 <?php
 require_once 'bootstrap.php';
 
-$templateParams["titolo"] = "Blog TW - Login";
-$templateParams["nome"] = "login-form.php";
+//Controllo se utente sta facendo login(se ricevo in post username e password)
+if(isset($_POST["username"]) && isset($_POST["password"])){
+    $login_result = $dbh->checkLogin($_POST["username"], $_POST["password"]);
+    if(count($login_result) == 0){
+        //Login fallito
+        $templateParams["errorelogin"] = "Errore! Controllare username o password";
+    }
+    else{
+        //Login con successo
+        registerLoggedUser($login_result[0]);
+    }
+}
+
+
+$templateParams["articoli"] = [];
+
+
+//Controllo se l'utente è loggato
+if(isUserLoggedIn()){
+    //Utente loggato
+    $templateParams["titolo"] = "Blog TW - Admin";
+    $templateParams["nome"] = "login-home.php";
+    $templateParams["articoli"] = $dbh->getPostByAuthorId($_SESSION["idautore"]);
+    if(isset($_GET["formmsg"])){
+        $templateParams["formmsg"] = $_GET["formmsg"];
+    }
+
+}
+else{
+    //Utente non loggato
+    $templateParams["titolo"] = "Blog TW - Login";
+    $templateParams["nome"] = "login-form.php";
+
+    
+}
+
+
+
+
 
 $templateParams["categorie"] = $dbh->getCategories();
 $templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
